1d4d85b0729d20e3dc61e723550d4e9d3a910410
Richard Purves <r.purves@arts.ac.uk>
2013-10-30 11:48:31 +0000

    Create ResetLocalAdminPW.sh

---
+++ ResetLocalAdminPW.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to reset uadmin account password via Casper policy
+
+# Author  : r.purves@arts.ac.uk
+# Version : 1.0 - Initial Version
+
+# Script relies on old and new passwords being passed via parameter 4.
+
+# Grab passed password
+newpassword=$4
+
+# Make the password change
+/usr/bin/dscl . passwd /Users/uadmin $newpassword
+
+exit 0
